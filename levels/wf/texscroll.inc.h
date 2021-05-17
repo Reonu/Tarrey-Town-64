bf608bcbfd45a28473e2cec4ffc747832378eaf6
Reonu <danileon95@gmail.com>
2021-05-18 00:44:54 +0100

    level changes

--- levels/wf/texscroll.inc.h
+++ levels/wf/texscroll.inc.h
@@ -1,3 +1,4 @@
+extern void scroll_wf_dl_Cube_001_mesh_layer_5_vtx_0();
 extern void scroll_wf_dl_LavaSemitransparent_mesh_layer_5_vtx_0();
 extern void scroll_wf_dl_Room_3_mesh_layer_1_vtx_2();
 extern void scroll_wf();
